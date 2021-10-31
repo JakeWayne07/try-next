5d45a26d5c374e3005fa58dea48234a7cafd4ee9
JakeWayne07 <waynemutai@gmail.com>
2021-10-31 19:53:13 +0300

    ✨ Added the Head

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Head from 'next/head'
 
 const about = () => {
     return (
         <div>
+        <Head>
+          <title>About Us</title>
+        </Head>
             <h1>More about Us</h1>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from 'next/head' //? When you want to use custom titles, metatags, k
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Welcome to the Future</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
       <h1>The Homepage</h1>
       <h2>Components</h2>
     </div>
