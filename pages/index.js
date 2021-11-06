97139c5c39de013ccf1f6b355e94d07d77924e67
JakeWayne07 <waynemutai@gmail.com>
2021-11-06 13:22:43 +0300

    ✨ Begin Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head' //? When you want to use custom titles, metatags, keywords etc
 
-export default function Home() {
+export default function Home({articles}) {
+
   return (
     <div>
       <Head>
@@ -11,4 +12,20 @@ export default function Home() {
       <h2>Components</h2>
     </div>
   )
+}
+
+//? Get Static Props -> Fetch Data at Build Time
+//? Get Server Side Props -> Fetch Data on every Request -> slower
+//? Get Static Paths Props -> Dynamically generate paths based on the data being fetched
+
+export const getStaticProps = async () =>{
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5`)
+
+  const articles = await res.json()
+
+  return {
+    props:{
+      articles
+    }
+  }
 }
